b4789073567c1c74490f09cca9d57822701be5ac
jerry tang <jerry.tang.haibiao@gmail.com>
2020-10-22 21:43:37 +0800

    1、添加小程序首页、分类接口

--- tacomall-api/tacomall-api-portal/src/main/java/store/tacomall/apiportal/strategy/impl/GoodsCategoryStrategy.java
+++ tacomall-api/tacomall-api-portal/src/main/java/store/tacomall/apiportal/strategy/impl/GoodsCategoryStrategy.java
@@ -2,7 +2,7 @@
 /***
  * @Author: 码上talk|RC
  * @Date: 2020-07-23 10:46:52
- * @LastEditTime: 2020-07-24 10:14:04
+ * @LastEditTime: 2020-10-22 21:31:33
  * @LastEditors: 码上talk|RC
  * @Description: 
  * @FilePath: /tacomall-springboot/tacomall-api/tacomall-api-portal/src/main/java/store/tacomall/apiportal/strategy/impl/GoodsCategoryStrategy.java
@@ -30,7 +30,7 @@ public class GoodsCategoryStrategy implements Strategy {
     public ResponseVo<Map<String, Object>> buildPage() {
         ResponseVo<Map<String, Object>> responseVo = new ResponseVo<>();
         Map<String, Object> map = new HashMap<>();
-        map.put("goodsCategory", this.goodsCategoryService.getGoodsCategories());
+        map.put("goodsCategory", this.goodsCategoryService.getGoodsCategories().getData());
         responseVo.setData(map);
         return responseVo;
     }
